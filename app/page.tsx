864902f3f848e091e386b0ee0c0d2d5e3ac036a3
Jean Lauliac <lauliacj@gmail.com>
2025-02-27 23:27:14 +0000

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,28 @@
-import { customColumnsTable, moleculesTable } from "@/db/schema";
-import db from "@/db";
-import { Filter, MoleculesTable, SortPredicate } from "./molecules-table";
-import { and, asc, count, desc, eq, ilike, SQL } from "drizzle-orm";
+import Link from "next/link";
 
-export default async function Home({
-  searchParams,
-}: {
-  // There are the query parameters passed to the page (?filters=..., etc).
-  // This is a Next.js convention, it will provide them to us out of the box.
-  searchParams: Promise<{ filters?: string; sorting?: string; page?: string }>;
-}) {
-  const { page, filters, sorting } = await searchParams;
+export default async function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
-      <header className="w-full h-10 bg-slate-50 border-b border-b-slate-100 px-4 py-2 font-bold">
-        rentwise.
+      <header className="fixed top-0 left-0 right-0 w-full h-12 bg-slate-50 border-b border-b-slate-100 px-4 py-2 font-bold text-lg z-10 shadow-sm flex items-center">
+        <Link href="/">rentwise.</Link>
       </header>
-      <main className="p-4">Boost Your Rental Income with Trusted Reviews</main>
+      <main className="p-4 mt-12">
+        <div className="flex mx-auto max-w-screen-lg pt-32">
+          <div className="w-1/2 flex flex-col gap-8">
+            <p className="text-4xl font-bold">
+              Boost your{" "}
+              <span className="text-blue-600 bg-slate-100 px-2">
+                rental income
+              </span>{" "}
+              with trusted reviews
+            </p>
+            <p className="text-xl">
+              Great tenants leave great reviews. Use RentWise to build trust and
+              command higher rents
+            </p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
