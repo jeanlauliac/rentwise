2a5f6395b23db402901acbae520f0764d3a372b9
Jean Lauliac <lauliacj@gmail.com>
2025-04-03 19:17:35 +0100

    nit

--- db/schema.ts
+++ db/schema.ts
@@ -1,8 +1,12 @@
 import { pgTable, varchar, timestamp, uuid } from "drizzle-orm/pg-core";
 import { authUsers } from "drizzle-orm/supabase";
 
-// FIXME: enable RLS for this table, see https://orm.drizzle.team/docs/rls
+// FIXME: maybe enable RLS for this table, see https://orm.drizzle.team/docs/rls
 // and https://github.com/drizzle-team/drizzle-orm/blob/main/drizzle-orm/src/supabase/rls.ts
+// and https://github.com/rphlmr/drizzle-supabase-rls/blob/main/database/schema.ts
+// Right this moment it's not absolutely required because I'm doing only
+// server-side fetches and rendering, and I disabled the Supabase Data API.
+//
 export const landlords = pgTable("landlords", {
   id: uuid("id")
     .primaryKey()
